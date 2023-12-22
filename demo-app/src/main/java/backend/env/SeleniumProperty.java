f8302582904150425477433b52c9081cfa96b666
juny76 <tuannguyenhuy87@gmail.com>
2023-12-22 11:46:40 +0700

    fix some constant

--- demo-app/src/main/java/backend/env/SeleniumProperty.java
+++
@@ -1,8 +0,0 @@
-package backend.env;
-
-public class SeleniumProperty {
-    public static final String driverPath = "chromedriver.exe";
-    public static final String extensionPath = "Adguard.crx";
-
-
-}

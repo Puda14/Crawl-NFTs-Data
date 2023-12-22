11c21b612b6c1c935cf3b3b024daec6199a337c8
luvu4ever <dungfixbug@gmail.com>
2023-12-22 23:07:03 +0700

    new change to app

--- demo-app/src/main/java/backend/crawler/impl/TwitterCrawler.java
+++ demo-app/src/main/java/backend/crawler/impl/TwitterCrawler.java
@@ -29,6 +29,7 @@ public class TwitterCrawler implements SeleniumCrawler<Tweet, Iterable<Tweet>> {
     private static final int MIN_REPLY = 0;
     private static final int FILTER_REPLIES = 0;
     private static final Double FIRST_RELOAD_CONDITION = 1000.0;
+    private static final Double LOADED_SOME_TWITTER = -100000.0;
     private static final String TWEET_XPATH = "//article[@data-testid='tweet']";
     private final TwitterInteraction webInteraction;
     private final SeleniumConfig seleniumConfig;
@@ -50,25 +51,29 @@ public class TwitterCrawler implements SeleniumCrawler<Tweet, Iterable<Tweet>> {
     @Override
     public List<Tweet> getWebsiteData(String keyword, String startDay, String endDay) {
         List<Tweet> tweets = new ArrayList<>();
-        AccountManager accountManager = new AccountManager(accountFilePath);
-        String accountDetails[] = accountManager.changeAccount();
-        String pUsername = accountDetails[0];
-        String pPassword = accountDetails[1];
+        AccountManager accountManager = new AccountManager("accountFilePath");
         WebDriver driver = seleniumConfig.initBrowser();
-        webInteraction.login(driver, pUsername, pPassword);
         while (startDay.compareTo(endDay) <= 0) {
-            webInteraction.search(driver, keyword, startDay, MIN_FAVES, MIN_RETWEET, MIN_REPLY, FILTER_REPLIES);
+            String accountDetails[] = accountManager.changeAccount("accountFilePath");
+            String pUsername = accountDetails[0];
+            String pPassword = accountDetails[1];
+            String pEmail = accountDetails[2];
+            webInteraction.login(driver, pUsername, pPassword, pEmail);
+            webInteraction.search(driver, keyword, startDay, endDay, MIN_FAVES, MIN_RETWEET, MIN_REPLY, FILTER_REPLIES);
 
             Double lastPosition = -2.0;
-//            Double currPosition = -2.0;
             try {
                 while (true) {
-//                    lastPosition = currPosition;
-//                    currPosition = webInteraction.scrollDown(driver);
                     lastPosition = webInteraction.scrollDown(driver);
-                    if (lastPosition.compareTo(FIRST_RELOAD_CONDITION * 1.0) < 0) {
+                    //if reload button detected, break
+                    if(lastPosition.compareTo(FIRST_RELOAD_CONDITION * 1.0) < 0 && reloadButtonDetected(driver,twitterProperty.getReloadButton()))
                         break;
-                    }
+                    //if end of page detected, break
+//                    if (lastPosition.compareTo(FIRST_RELOAD_CONDITION * 1.0) < 0)
+//                        break;
+                    //if cant scroll down anymore, break
+                    if(lastPosition.compareTo(0.0) < 0)
+                        break;
                     List<WebElement> articles = driver.findElements(By.xpath(TWEET_XPATH));
                     for (WebElement article : articles) {
                         if (isAdvertisement(article))
@@ -86,28 +91,26 @@ public class TwitterCrawler implements SeleniumCrawler<Tweet, Iterable<Tweet>> {
                 Thread.currentThread().interrupt();
                 e.printStackTrace();
             }
-//            System.out.println("last position: " + lastPosition + " " + currPosition);
+            System.out.println("last position: " + lastPosition);
 //            if(reloadButtonDetected(driver,twitterProperty.getReloadButton())){
-//                webInteraction.logout(driver);
-//                accountDetails = accountManager.changeAccount();
-//                pUsername = accountDetails[0];
-//                pPassword = accountDetails[1];
-//                System.out.println("change account to " + pUsername + " " + pPassword);
-//                driver.manage().deleteAllCookies();
-//                webInteraction.login(driver, pUsername, pPassword);
+
+            if(lastPosition.compareTo(LOADED_SOME_TWITTER) < 0 || noResultDetected(driver, twitterProperty.getNoResult()))
+
+                startDay = addDayToString(startDay, (DAY_GAP + 1));
+            else{
+                if(!reloadButtonDetected(driver,twitterProperty.getReloadButton()))
+                    startDay = addDayToString(startDay, (DAY_GAP + 1));
+            }
+            if (startDay.compareTo(endDay) > 0) break;
+            System.out.println(startDay + " " + endDay);
+            webInteraction.logout(driver);
+
 //                continue;
+
 //            }
 
-            startDay = addDayToString(startDay, DAY_GAP + 1);
-            if (startDay.compareTo(endDay) > 0) break;
+//            startDay = addDayToString(startDay, DAY_GAP + 1);
 //            System.out.println(startDay + " " + endDay);
-            webInteraction.logout(driver);
-            accountDetails = accountManager.changeAccount();
-            pUsername = accountDetails[0];
-            pPassword = accountDetails[1];
-            System.out.println("change account to " + pUsername + " " + pPassword);
-            driver.manage().deleteAllCookies();
-            webInteraction.login(driver, pUsername, pPassword);
         }
         driver.quit();
         return tweets;
